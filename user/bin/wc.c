f92f2e1833648dc40cba7cd98969d8e8e9a51225
Ezyrone <grz.jory38@gmail.com>
2026-02-16 10:29:49 +0100

    feat(wc): Add start code (to continue)

--- user/bin/wc.c
+++ user/bin/wc.c
@@ -12,3 +12,18 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct {
+    unsigned long long lines;
+    unsigned long long words;
+    unsigned long long bytes;
+} wc_counts_t;
+
+static void add_counts(wc_counts_t *dst, const wc_counts_t *src) {
+    dst->lines += src->lines;
+    dst->words += src->words;
+    dst->bytes += src->bytes;
+}
+
+static int is_space_byte(unsigned char c) {
+    return isspace((int)c);
+}
