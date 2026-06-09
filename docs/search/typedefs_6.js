48639b721e3051bc3df9eb6d8d28780e4499895b
Darleanow <Darleanow@users.noreply.github.com>
2026-06-09 01:21:08 +0000

    chore: update pages [coverage + docs] @ db7e5e6cd675ca8c7e965105cae49f9b0ac4b306 db7e5e6cd675ca8c7e965105cae49f9b0ac4b306

--- docs/search/typedefs_6.js
+++ docs/search/typedefs_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['keyboard_5fhandler_5ft_0',['keyboard_handler_t',['../keyboard_8h.html#a01ebe4112320aa842f18b4c1e9ce1ad6',1,'keyboard.h']]]
+  ['kern_5ferr_5ft_0',['kern_err_t',['../types_8h.html#a60161a130eae0ecc7257b79ac7418c41',1,'types.h']]],
+  ['keyboard_5fhandler_5ft_1',['keyboard_handler_t',['../keyboard_8h.html#a01ebe4112320aa842f18b4c1e9ce1ad6',1,'keyboard.h']]]
 ];
